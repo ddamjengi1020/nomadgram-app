01131bba677fd6eab4ecbbfb58733a4d5423ff7a
ddamjengi1020 <silluat11@gmail.com>
2020-12-23 21:16:36 +0900

    #1.0 Preload setting that font, cache etc.

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { persistCache, AsyncStorageWrapper } from "apollo3-cache-persist";
+import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import apolloClientOptions from "./apollo";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [loaded, setLoaded] = useState(false);
+  const [clientS, setClientS] = useState(null);
+  const preLoad = async () => {
+    try {
+      await Font.loadAsync({
+        ...Ionicons.font,
+      });
+      const cache = new InMemoryCache();
+
+      await persistCache({
+        cache,
+        storage: new AsyncStorageWrapper(AsyncStorage),
+      });
+
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions,
+      });
+      setClientS(client);
+      setLoaded(true);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    preLoad();
+  }, []);
+
+  return loaded && clientS ? (
+    <ApolloProvider client={clientS}>
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </ApolloProvider>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
